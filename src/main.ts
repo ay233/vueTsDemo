78350613877b41ec57127ca5a3a092eb32423376
anyao <ay12580233@peitian.com>
2019-08-15 11:14:47 +0800

    add ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
+import "./registerServiceWorker";
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import "./registerServiceWorker";
-
+import './plugins/element.js';
+//通配css
+import "normalize.css/normalize.css";
+import '@/plugins/zepto.js';
+// import axios from './utils/http.js';
+import echarts from 'echarts';
+import '@/permission';// permission control
+// Vue.prototype.axios = axios;
+Vue.prototype.$echarts = echarts;
+const env = process.env;
+//后台静态资源地址
+Vue.prototype.imgbaseUrl = env.VUE_APP_imgServer;
+//后台上传接口服务地址
+Vue.prototype.uploadbaseUrl = env.VUE_APP_server;
+//开发不显示提示
 Vue.config.productionTip = false;
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,13 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+	<h1>home页</h1>    
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
-@Component({
-  components: {
-    HelloWorld
-  }
-})
-export default class Home extends Vue {}
+export default class Home extends Vue {
+
+}
 </script>
